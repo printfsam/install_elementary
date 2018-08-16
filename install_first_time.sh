6c15cea1c1148fba1afb6903f1eec4673b6accf1
Sam <xsamtheman11@gmail.com>
2018-08-15 21:01:18 -0400

    added sublime

--- install_first_time.sh
+++ install_first_time.sh
@@ -10,3 +10,9 @@ apt-get install elementary-tweaks
 add-apt-repository ppa:nathan-renniewaldock/flux
 apt-get update
 apt-get install fluxgui
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+apt-get install apt-transport-https
+echo "deb https://download.sublimetext.com/ apt/stable/" | tee /etc/apt/sources.list.d/sublime-text.list
+apt-get update
+apt-get install sublime-text
+
